2e5356f26c420cbcff46b061fcdf1f2f5196cbb5
perwriters <perwriters@gmail.com>
2024-08-02 22:11:56 +0300

    first commit

---
+++ MySQLServer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+try:
+
+    #connect with database
+
+    connection = mysql.connector.connect(
+
+        host = "localhost",
+        user = "root",
+        password = "22427843Tk!!?"
+    )
+
+    mycursor = connection.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+    mycursor.close()
+    connection.close()
+
+except mysql.connector.Error as e:
+    print(f"Error:", e)
---
+++ alx_book_store.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+CREATE TABLE Books (
+    book_id INT AUTO INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+);
+
+CREATE TABLE Authors (
+    author_id INT AUTO INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+    customer_id INT AUTO INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    customer_email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INT AUTO INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+    order_details_id INT AUTO INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
